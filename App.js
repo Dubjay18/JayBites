28227b303852295bd952982b83f0d2cb173553c9
Dubjay18 <jejeniyi7@gmail.com>
2023-01-22 00:05:29 +0100

    started core features 2

--- App.js
+++ App.js
@@ -18,8 +18,25 @@ import { Searchbar } from "react-native-paper";
 import { ThemeProvider } from "styled-components/native";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 import { theme } from "./src/infrastructure/theme";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 const isAndroid = Platform.OS === "android";
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
 export default function App() {
   let [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -34,10 +51,31 @@ export default function App() {
       </View>
     );
   }
+  const Map = () => <Text>map</Text>;
+  const Settings = () => <Text>settings</Text>;
+  const Tab = createBottomTabNavigator();
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={createScreenOptions}
+            tabBarOptions={{
+              activeTintColor: "tomato",
+              inactiveTintColor: "gray",
+            }}>
+            <Tab.Screen
+              name='Restaurants'
+              component={RestaurantsScreen}
+            />
+            <Tab.Screen name='Map' component={Map} />
+            <Tab.Screen
+              name='Settings'
+              component={Settings}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <ExpoStatusBar style='auto' />
     </>
